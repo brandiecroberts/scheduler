7c831692ae8142f445472085c13d951f9caaac33
Brandie Roberts <brandiecroberts@gmail.com>
2023-02-21 15:16:38 -0800

    DayList created and implemented useState

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,12 +7,23 @@ const dayClass = classNames('day', {
   'day-list__item': props.name,
   'day-list__item--selected': props.selected === true,
   'day-list__item--full': props.spots === 0
-})
+});
+
+const formatSpots = () => {
+if (props.spots === 0) {
+  return <span>no spots remaining</span>
+} else if (props.spots === 1) {
+  return <span>1 spot remaining</span>
+}
+ return (
+  <span>{props.spots} spots remaining</span>
+ )
+};
 
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining...</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
